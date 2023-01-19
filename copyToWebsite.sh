330962c7801c09f35a01a872db03623d4149df02
Teebs Teebson <teebs.teebson@gmail.com>
2023-01-18 21:48:55 -0700

    Misc changes to get sourcemaps working on website

--- copyToWebsite.sh
+++ copyToWebsite.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-SRC='./bin'
+BIN='./bin'
+SRC='./src'
 DEST='../brian-putnam.com/www/wordle'
 
-files=(compare.js iter.js state_filters.js wordlist.js state_filters.js)
+files=(compare iter state_filters wordlist state_filters)
 
 npm run build
 for i in "${files[@]}"; do
-    cmd="cp $SRC/$i $DEST/"
+    cmd="cp $BIN/$i.js $DEST/"
     echo $cmd
     $($cmd)
 
-    cmd="cp $SRC/$i.map $DEST/"
+    cmd="cp $SRC/$i.ts $DEST/"
+    echo $cmd
+    $($cmd)
+
+    cmd="cp $BIN/$i.js.map $DEST/"
     echo $cmd
     $($cmd)
 done
